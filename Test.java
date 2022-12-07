f3c0205545fb59a6df3230d7f474b3d90b857a78
Maryam313 <maryamajmi313@gmail.com>
2022-12-07 12:18:53 +0400

    Added Hello world program with compiled byte file

---
+++ Test.java
@@ -0,0 +1,11 @@
+class Test {
+	public static void main(String[] args){
+		
+	      // System.out.println("Hello World");
+	   	   System.out.println("Select an Action:");
+		   System.out.println("----------------------");
+	   	   System.out.println("1)-Generate HASH");
+		   System.out.println("2)-Compare HASH");
+		   System.out.println("3)-Re-compute");
+	}
+}
